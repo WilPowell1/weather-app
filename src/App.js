194a9a95789d3749c421a478a115501b8c8b5af6
WilPowell1 <powellwil88@gmail.com>
2020-08-05 18:32:18 +0100

    forecast-summaries error

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import LocationDetails from './components/location-details';
 import PropTypes from 'prop-types';
+import LocationDetails from './components/location-details';
+import ForecastSummaries from './components/forecast-summaries';
 
 const App = props => 
+<div className="forecast">
   <LocationDetails 
     city={props.location.city}
     country={props.location.country}
   />
+
+  <ForecastSummaries forecasts={props.forecasts}/>
+</div>
 
 App.propTypes = {
   location: PropTypes.shape({
     city: PropTypes.string,
     country: PropTypes.string,
   }).isRequired,
+  forecasts: PropTypes.array.isRequired,
 };
 
 export default App;
