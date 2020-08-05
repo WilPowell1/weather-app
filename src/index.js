3442f2a26bec1d4d01d8928c746e38e1ebfbe548
WilPowell1 <powellwil88@gmail.com>
2020-08-05 16:29:53 +0100

    location-details component

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
+import { location } from './data/forecast.json';
+
+render(
+  <App location={location} />, document.getElementById('root')
+);
